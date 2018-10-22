6fcbb247cc5192fe978abe87f31a5031c948ec90
Mayur Mathur <moneymayur@users.noreply.github.com>
2018-10-22 19:06:28 +0530

    Update Reco.java

--- SimpleSpringBoot/com/sample/main/Reco.java
+++ SimpleSpringBoot/com/sample/main/Reco.java
@@ -2,11 +2,12 @@ package com.sample
 
 @SpringBootApplication
 public class Reco {
-public static void main(String[] args){
-new SpringApplicationBuilder(Reco.class).web(true).run(args);
-}
-@Bean
-public RestTemplate restTemplate(){
-return new RestTemplate();
-}
+  public static void main(String[] args){
+    new SpringApplicationBuilder(Reco.class).web(true).run(args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(){
+    return new RestTemplate();
+  }
 }
